e795d4e89ec8b84c13a61175e2a231887c02f439
74th <site@j74th.com>
2015-11-23 08:12:12 +0900

    small fix

--- src/motion/AbstractMotion.ts
+++ src/motion/AbstractMotion.ts
@@ -2,19 +2,19 @@ import {IMotion} from "./IMotion"
 import {IEditor} from "../IEditor"
 import {Position} from "../VimStyle"
 
-export class AbstractMotion implements IMotion{
-	
-	private count:number;
-	
-	public GetCount(){
-		return this.count;
-	}
-	
-	public SetCount(count:number){
-		this.count = count;
-	}
-	
-	public CalculateEnd(editor:IEditor,start:Position){
-		throw new Error("UnImplemented");
-	}
+export class AbstractMotion implements IMotion {
+
+    private count: number;
+
+    public GetCount() {
+        return this.count;
+    }
+
+    public SetCount(count: number) {
+        this.count = count;
+    }
+
+    public CalculateEnd(editor: IEditor, start: Position): Position {
+        throw new Error("UnImplemented");
+    }
 }
