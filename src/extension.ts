d31cc86c09e25bb2ecb9a50d9d9c109d3b443ab3
74th <site@j74th.com>
2015-11-22 23:15:39 +0900

    テストコードが残ってた

--- src/extension.ts
+++ src/extension.ts
@@ -10,7 +10,7 @@ export function activate(context: vscode.ExtensionContext) {
     var isJisKeyboard: boolean;
     var conf = vscode.workspace.getConfiguration('vimStyle');
     isJisKeyboard = conf.get<boolean>("useJisKeyboard");
-    isJisKeyboard = true;
+    
     var disposable = vscode.commands.registerCommand('vim.a', () => {
         vim.PushKey(VimStyleEnums.Key.a);
     });
