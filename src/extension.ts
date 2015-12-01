ba46d1b9e23441b27aff8a5dda8689065b5cae31
74th <site@j74th.com>
2015-12-01 18:52:26 +0900

    support JIS keyboard bindings for windows

--- src/extension.ts
+++ src/extension.ts
@@ -5,26 +5,26 @@ import {VSCodeEditor} from './VSCodeEditor'
 export function activate(context: vscode.ExtensionContext) {
 
     var conf = vscode.workspace.getConfiguration('vimStyle');
-    var isJisKeyboard = conf.get<boolean>('useJisKeyboard', false);
+    var isWinJisKeyboard = conf.get<boolean>('useWinJisKeyboard', false);
     var showMode = conf.get<boolean>('showMode', false);
-    
+
     var editor = new VSCodeEditor({
         showMode: showMode
     });
-    
+
     var vim = new VimStyle(editor);
-    
+
     context.subscriptions.push(editor);
-    
+
     var disposable = vscode.workspace.onDidChangeConfiguration(() => {
         conf = vscode.workspace.getConfiguration('vimStyle');
         showMode = conf.get<boolean>('showMode', false);
-        
+
         editor.SetModeStatusVisibility(showMode);
     });
-    
+
     context.subscriptions.push(disposable);
-    
+
     disposable = vscode.commands.registerCommand('vim.a', () => {
         vim.PushKey(Key.a);
     });
@@ -274,284 +274,240 @@ export function activate(context: vscode.ExtensionContext) {
     });
     context.subscriptions.push(disposable);
     disposable = vscode.commands.registerCommand('vim.GA', () => {
-        if (isJisKeyboard) {
-            // JIS: TODO
-            vim.PushKey(Key.GraveAccent);
+        if (isWinJisKeyboard) {
+            // @
+            vim.PushKey(Key.AtMark);
         } else {
-            // US :
+            // ^
             vim.PushKey(Key.GraveAccent);
         }
     });
     context.subscriptions.push(disposable);
     disposable = vscode.commands.registerCommand('vim.SGA', () => {
-        // JIS: Shift+^(~) Shift+@(`) BUG?
-        //      Shift+^で到達（まぁ良い）
-        // US :
-        vim.PushKey(Key.Tilde);
+        if (isWinJisKeyboard) {
+            // `
+            vim.PushKey(Key.GraveAccent);
+        } else {
+            // ~
+            vim.PushKey(Key.Tilde);
+        }
     });
     context.subscriptions.push(disposable);
     disposable = vscode.commands.registerCommand('vim.S1', () => {
-        // JIS: Shift+1
-        // US :
+        // !
         vim.PushKey(Key.Exclamation);
     });
     context.subscriptions.push(disposable);
     disposable = vscode.commands.registerCommand('vim.S2', () => {
-        if (isJisKeyboard) {
-            // JIS: BUG? 到達できない
+        if (isWinJisKeyboard) {
+            // "
             vim.PushKey(Key.Quotation);
         } else {
-            // US :
+            // @
             vim.PushKey(Key.AtMark);
         }
     });
     context.subscriptions.push(disposable);
     disposable = vscode.commands.registerCommand('vim.S3', () => {
-        // JIS: Shift+3
-        // US :
+        // #
         vim.PushKey(Key.Sharp);
     });
     context.subscriptions.push(disposable);
     disposable = vscode.commands.registerCommand('vim.S4', () => {
-        // JIS: Shift+4
-        // US :
+        // $
         vim.PushKey(Key.Doller);
     });
     context.subscriptions.push(disposable);
     disposable = vscode.commands.registerCommand('vim.S5', () => {
-        // JIS: Shift+5
-        // US :
+        // %
         vim.PushKey(Key.Percent);
     });
     context.subscriptions.push(disposable);
     disposable = vscode.commands.registerCommand('vim.S6', () => {
-        if (isJisKeyboard) {
-            // JIS: Shift+6
+        if (isWinJisKeyboard) {
+            // &
             vim.PushKey(Key.Ampersand);
         } else {
-            // US :
+            // ^
             vim.PushKey(Key.CircumflexAccent);
         }
     });
     context.subscriptions.push(disposable);
     disposable = vscode.commands.registerCommand('vim.S7', () => {
-        if (isJisKeyboard) {
-            // JIS: BUG? ここに到達できない
+        if (isWinJisKeyboard) {
+            // '
             vim.PushKey(Key.Apostrophe);
         } else {
-            // US :
+            // &
             vim.PushKey(Key.Ampersand);
         }
     });
     context.subscriptions.push(disposable);
     disposable = vscode.commands.registerCommand('vim.S8', () => {
-        if (isJisKeyboard) {
-            // JIS: Shift+8
+        if (isWinJisKeyboard) {
+            // (
             vim.PushKey(Key.LeftParenthesis);
         } else {
-            // US :
+            // @
             vim.PushKey(Key.Asterisk);
         }
     });
     context.subscriptions.push(disposable);
     disposable = vscode.commands.registerCommand('vim.S9', () => {
-        if (isJisKeyboard) {
-            // JIS: Shift+9
+        if (isWinJisKeyboard) {
+            // )
             vim.PushKey(Key.RightParenthesis);
         } else {
-            // US :
+            // (
             vim.PushKey(Key.LeftParenthesis);
         }
     });
     context.subscriptions.push(disposable);
     disposable = vscode.commands.registerCommand('vim.S0', () => {
-        if (isJisKeyboard) {
-            // JIS: Shift+0
-            vim.PushKey(Key.n0);
+        if (isWinJisKeyboard) {
+            // _
+            vim.PushKey(Key.LowLine);
         } else {
-            // US :
+            // )
             vim.PushKey(Key.RightParenthesis);
         }
     });
     disposable = vscode.commands.registerCommand('vim.Hy', () => {
-        // JIS: -
-        //      _(BUG!)
-        // US :
+        // -
         vim.PushKey(Key.Hyphen);
     });
     context.subscriptions.push(disposable);
     disposable = vscode.commands.registerCommand('vim.SHp', () => {
-        if (isJisKeyboard) {
-            // JIS: Shift+_(BUG?)
+        if (isWinJisKeyboard) {
+            // =
             vim.PushKey(Key.Equals);
         } else {
-            // US :
+            // _
             vim.PushKey(Key.LowLine);
         }
     });
     context.subscriptions.push(disposable);
     disposable = vscode.commands.registerCommand('vim.Eq', () => {
-        if (isJisKeyboard) {
-            // JIS: ^ BUG?
-            vim.PushKey(Key.CircumflexAccent);
+        if (isWinJisKeyboard) {
+            // ;
+            vim.PushKey(Key.Semicolon);
         } else {
-            // US :
+            // =
             vim.PushKey(Key.Equals);
         }
     });
     context.subscriptions.push(disposable);
     disposable = vscode.commands.registerCommand('vim.SEq', () => {
-        if (isJisKeyboard) {
-            // JIS: Shift+-(=)
-            //      Shift++(+) BUG?
-            vim.PushKey(Key.Equals);
-        } else {
-            // US :
-            vim.PushKey(Key.Plus);
-        }
+        // +
+        vim.PushKey(Key.Plus);
     });
     context.subscriptions.push(disposable);
     disposable = vscode.commands.registerCommand('vim.LB', () => {
-        if (isJisKeyboard) {
-            // JIS: [
-            //      @ BUG?
-            vim.PushKey(Key.LeftSquareBracket);
-        } else {
-            // US :
-            vim.PushKey(Key.LeftSquareBracket);
-        }
+        // [
+        vim.PushKey(Key.LeftSquareBracket);
     });
     context.subscriptions.push(disposable);
     disposable = vscode.commands.registerCommand('vim.SLB', () => {
-        if (isJisKeyboard) {
-            // JIS: 到達しない
-            vim.PushKey(Key.LeftCurlyBracket);
-        } else {
-            // US :
-            vim.PushKey(Key.LeftCurlyBracket);
-        }
+        // {
+        vim.PushKey(Key.LeftCurlyBracket);
     });
     context.subscriptions.push(disposable);
     disposable = vscode.commands.registerCommand('vim.RB', () => {
-        if (isJisKeyboard) {
-            // JIS: ]
-            vim.PushKey(Key.RightSquareBracket);
-        } else {
-            // US :
-            vim.PushKey(Key.RightSquareBracket);
-        }
+        // ]
+        vim.PushKey(Key.RightSquareBracket);
     });
     context.subscriptions.push(disposable);
     disposable = vscode.commands.registerCommand('vim.SRB', () => {
-        if (isJisKeyboard) {
-            // JIS: Shift+](})
-            vim.PushKey(Key.RightCurlyBracket);
-        } else {
-            // US : 
-            vim.PushKey(Key.RightCurlyBracket);
-        }
+        // ]
+        vim.PushKey(Key.RightCurlyBracket);
     });
     context.subscriptions.push(disposable);
     disposable = vscode.commands.registerCommand('vim.RS', () => {
-        if (isJisKeyboard) {
-            // JIS: 動作しない
-            vim.PushKey(Key.ReverseSolidus);
-        } else {
-            vim.PushKey(Key.ReverseSolidus);
-        }
+        // \
+        vim.PushKey(Key.ReverseSolidus);
     });
     context.subscriptions.push(disposable);
     disposable = vscode.commands.registerCommand('vim.SRS', () => {
-        if(isJisKeyboard){
-            // JIS: 動作しない
-            vim.PushKey(Key.VerticalLine);
-        }else{
-            vim.PushKey(Key.VerticalLine);
-        }
+        // |
+        vim.PushKey(Key.VerticalLine);
     });
     context.subscriptions.push(disposable);
     disposable = vscode.commands.registerCommand('vim.Sc', () => {
-        // JIS: ;
-        // US : 
-        vim.PushKey(Key.Semicolon);
+        if (isWinJisKeyboard) {
+            // :
+            vim.PushKey(Key.Colon);
+        } else {
+            // ;
+            vim.PushKey(Key.Semicolon);
+        }
     });
     context.subscriptions.push(disposable);
     disposable = vscode.commands.registerCommand('vim.SSc', () => {
-        if (isJisKeyboard) {
-            // JIS: 到達しない
-            vim.PushKey(Key.Plus);
+        if (isWinJisKeyboard) {
+            // *
+            vim.PushKey(Key.Asterisk);
         } else {
-            // US :
+            // :
             vim.PushKey(Key.Colon);
         }
     });
     context.subscriptions.push(disposable);
     disposable = vscode.commands.registerCommand('vim.Ap', () => {
-        if (isJisKeyboard) {
-            // JIS: 到達しない！
-            vim.PushKey(Key.Apostrophe);
+        if (isWinJisKeyboard) {
+            // ^
+            vim.PushKey(Key.CircumflexAccent);
         } else {
-            // US :
+            // '
             vim.PushKey(Key.Apostrophe);
         }
     });
     context.subscriptions.push(disposable);
     disposable = vscode.commands.registerCommand('vim.SAp', () => {
-        if (isJisKeyboard) {
-            // JIS: Shift+2(")
-            //      SHift+7(') BUG?
-            vim.PushKey(Key.Quotation);
+        if (isWinJisKeyboard) {
+            // ~
+            vim.PushKey(Key.Tilde);
         } else {
-            // US :
+            // "
             vim.PushKey(Key.Quotation);
         }
     });
     context.subscriptions.push(disposable);
     disposable = vscode.commands.registerCommand('vim.Cm', () => {
-        // JIS: ,
-        // US :
+        // ,
         vim.PushKey(Key.Comma);
     });
     context.subscriptions.push(disposable);
     disposable = vscode.commands.registerCommand('vim.LT', () => {
-        // JIS: Shift+.(<)
-        // US :
+        // <
         vim.PushKey(Key.LessThan);
     });
     context.subscriptions.push(disposable);
     disposable = vscode.commands.registerCommand('vim.Pd', () => {
-        // JIS: .
-        // US :
+        // .
         vim.PushKey(Key.Period);
     });
     context.subscriptions.push(disposable);
     disposable = vscode.commands.registerCommand('vim.SPd', () => {
-        // JIS: Shift+.(>)
-        // US :
+        // >
         vim.PushKey(Key.GreaterThan);
     });
     context.subscriptions.push(disposable);
     disposable = vscode.commands.registerCommand('vim.Sl', () => {
-        // JIS: /
-        // US :
+        // /
         vim.PushKey(Key.Solidus);
     });
     context.subscriptions.push(disposable);
     disposable = vscode.commands.registerCommand('vim.SSl', () => {
-        // JIS: Shift+/(?)
-        // US :
+        // ?
         vim.PushKey(Key.Question);
     });
     context.subscriptions.push(disposable);
     disposable = vscode.commands.registerCommand('vim.Sp', () => {
-        // JIS: 到達しない BUG!
-        // US : 
+        // " "
         vim.PushKey(Key.Space);
     });
     context.subscriptions.push(disposable);
     disposable = vscode.commands.registerCommand('vim.Ec', () => {
-        // JIS: ESC
-        // US :
         vim.PushEscKey();
         vscode.commands.executeCommand("hideSuggestWidget");
     });
