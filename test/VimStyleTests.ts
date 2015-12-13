9f3000eded8a50cca9df82277fd00b32d26da037
74th <site@j74th.com>
2015-12-13 22:05:16 +0900

    insert mode tests

--- test/VimStyleTests.ts
+++ test/VimStyleTests.ts
@@ -45,10 +45,25 @@ export let VimStyleTests = {
         // TODO UP
     },
     "Insert Mode": {
-        "insert char": {
+        "i:insert charactors": {
             "in": ["aa|aa"],
             "key": "ibc_",
             "out": ["aab|caa"]
+        },
+        "a:append charctors":{
+            "in":["aa|aa"],
+            "key": "abc_",
+            "out":["aaab|c"]
+        },
+        "I:insert charactors to home of a line":{
+            "in":["aa|aa"],
+            "key":"Ibc_",
+            "out":["b|caaaa"]
+        },
+        "A:append charactors to end of a line":{
+            "in":["aa|aa"],
+            "key":"Abc_",
+            "out":["aaaab|c"]
         }
     }
 };
