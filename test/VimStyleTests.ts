e90ac98723aa94b877a508d386af08f837be3b36
74th <site@j74th.com>
2016-03-21 09:31:30 +0900

    refactor tests files

--- test/VimStyleTests.ts
+++ test/VimStyleTests.ts
@@ -1,34 +1,37 @@
-export let VimStyleTests = {
-    "Spec": {
-        "Basic": {
-            "in": ["abc|def"],
-            "key": "",
-            "out": ["abc|def"]
-        }
+export let VimStyleTests = {};
+
+VimStyleTests["Spec"] = {
+    "Basic": {
+        "in": ["abc|def"],
+        "key": "",
+        "out": ["abc|def"]
+    }
+};
+
+VimStyleTests["Right Motion"] = {
+    "l:move to right 1": {
+        "in": ["abc| def"],
+        "key": "l",
+        "out": ["abc |def"]
     },
-    "Right Motion": {
-        "l:move to right 1": {
-            "in": ["abc| def"],
-            "key": "l",
-            "out": ["abc |def"]
-        },
-        "3l:move to right 3": {
-            "in": ["a|bcdefg"],
-            "key": "3l",
-            "out": ["abcd|efg"]
-        },
-        "h:move to left 1": {
-            "in": ["abc| def"],
-            "key": "h",
-            "out": ["ab|c def"]
-        },
-        "3h:move to left 3": {
-            "in": ["abcdef|g"],
-            "key": "3h",
-            "out": ["abc|defg"]
-        }
+    "3l:move to right 3": {
+        "in": ["a|bcdefg"],
+        "key": "3l",
+        "out": ["abcd|efg"]
     },
-    "Word motion": {
+    "h:move to left 1": {
+        "in": ["abc| def"],
+        "key": "h",
+        "out": ["ab|c def"]
+    },
+    "3h:move to left 3": {
+        "in": ["abcdef|g"],
+        "key": "3h",
+        "out": ["abc|defg"]
+    }
+};
+
+VimStyleTests["Word motion"] = {
         "2w:move to next word": {
             "in": ["ab|c abc abc abc"],
             "key": "2w",
@@ -167,94 +170,93 @@ export let VimStyleTests = {
                 "ab|d abd"
             ],
         },
+    }
+VimStyleTests["Down Motion"] = {
+    "j:move to down 1": {
+        "in": [
+            "aaaaaaa",
+            "bbbb|bbb",
+            "ccccccc"
+        ],
+        "key": "j",
+        "out": [
+            "aaaaaaa",
+            "bbbbbbb",
+            "cccc|ccc"
+        ]
     },
-    "Down Motion": {
-        "j:move to down 1": {
-            "in": [
-                "aaaaaaa",
-                "bbbb|bbb",
-                "ccccccc"
-            ],
-            "key": "j",
-            "out": [
-                "aaaaaaa",
-                "bbbbbbb",
-                "cccc|ccc"
-            ]
-        },
-        "k:move to prev line": {
-            "in": [
-                "aaaa",
-                "bb|bb",
-                "cccc"
-            ],
-            "key": "k",
-            "out": [
-                "aa|aa",
-                "bbbb",
-                "cccc"
-            ]
-        }
-        // TODO UP
+    "k:move to prev line": {
+        "in": [
+            "aaaa",
+            "bb|bb",
+            "cccc"
+        ],
+        "key": "k",
+        "out": [
+            "aa|aa",
+            "bbbb",
+            "cccc"
+        ]
+    }
+    // TODO UP
+};
+VimStyleTests["Forward char motion"] = {
+    "fx:move to the charactor": {
+        "in": [
+            "ab|cdefghi"
+        ],
+        "key": "ff",
+        "out": [
+            "abcde|fghi"
+        ]
     },
-    "Forward char motion": {
-        "fx:move to the charactor": {
-            "in": [
-                "ab|cdefghi"
-            ],
-            "key": "ff",
-            "out": [
-                "abcde|fghi"
-            ]
-        },
-        "tx:move to before the charactor": {
-            "in": [
-                "ab|cdefghi"
-            ],
-            "key": "tf",
-            "out": [
-                "abcd|efghi"
-            ]
-        },
-        "Fx:move to the charactor": {
-            "in": [
-                "abcdefg|hi"
-            ],
-            "key": "Fc",
-            "out": [
-                "ab|cdefghi"
-            ]
-        },
-        "Tx:move to before the charactor": {
-            "in": [
-                "abcdefg|hi"
-            ],
-            "key": "Tc",
-            "out": [
-                "abc|defghi"
-            ]
-        }
+    "tx:move to before the charactor": {
+        "in": [
+            "ab|cdefghi"
+        ],
+        "key": "tf",
+        "out": [
+            "abcd|efghi"
+        ]
     },
-    "Insert Mode": {
-        "i:insert charactors": {
-            "in": ["aa|aa"],
-            "key": "ibc_",
-            "out": ["aab|caa"]
-        },
-        "a:append charctors": {
-            "in": ["aa|aa"],
-            "key": "abc_",
-            "out": ["aaab|ca"]
-        },
-        "I:insert charactors to home of a line": {
-            "in": ["aa|aa"],
-            "key": "Ibc_",
-            "out": ["b|caaaa"]
-        },
-        "A:append charactors to end of a line": {
-            "in": ["aa|aa"],
-            "key": "Abc_",
-            "out": ["aaaab|c"]
-        }
+    "Fx:move to the charactor": {
+        "in": [
+            "abcdefg|hi"
+        ],
+        "key": "Fc",
+        "out": [
+            "ab|cdefghi"
+        ]
+    },
+    "Tx:move to before the charactor": {
+        "in": [
+            "abcdefg|hi"
+        ],
+        "key": "Tc",
+        "out": [
+            "abc|defghi"
+        ]
+    }
+};
+VimStyleTests["Insert Mode"] = {
+    "i:insert charactors": {
+        "in": ["aa|aa"],
+        "key": "ibc_",
+        "out": ["aab|caa"]
+    },
+    "a:append charctors": {
+        "in": ["aa|aa"],
+        "key": "abc_",
+        "out": ["aaab|ca"]
+    },
+    "I:insert charactors to home of a line": {
+        "in": ["aa|aa"],
+        "key": "Ibc_",
+        "out": ["b|caaaa"]
+    },
+    "A:append charactors to end of a line": {
+        "in": ["aa|aa"],
+        "key": "Abc_",
+        "out": ["aaaab|c"]
     }
 };
