a77e9b5992c7891bfb6127cc67cf8767e3c3e9fa
74th <site@j74th.com>
2016-03-26 11:39:53 +0900

    add o test

--- test/vim/RepeatTests.ts
+++ test/vim/RepeatTests.ts
@@ -1,13 +1,35 @@
 export let RepeatTests = {};
 
 RepeatTests["repeat insert"] = {
-    "add text": {
+    "add text to current position": {
         "in": [
             "|abc",
         ],
         "key": "adef_$.",
         "out": [
             "adefbcde|f",
+        ]
+    },
+    "add text to new line": {
+        "in": [
+            "a|bc"
+        ],
+        "key": "odef_.",
+        "out": [
+            "abc",
+            "def",
+            "de|f"
+        ]
+    }
+};
+RepeatTests["repeat normal command"] = {
+    "add text": {
+        "in": [
+            "aaa |bbb ccc ddd",
+        ],
+        "key": "dw.",
+        "out": [
+            "aaa |ddd",
         ]
     }
 };
