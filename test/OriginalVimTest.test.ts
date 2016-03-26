b9f64f45b58d523dce1eff9f3d48510578ed3a29
74th <site@j74th.com>
2016-03-26 12:26:57 +0900

    update test

--- test/OriginalVimTest.test.ts
+++ test/OriginalVimTest.test.ts
@@ -33,7 +33,6 @@ for (let target in VimTests) {
                         let i = 0;
                         fs.readFile("OriginalVimOutput", function(err, text) {
                             let out = text.toString().split("\n");
-                            assert.equal(out.length - 1, spec["out"].length);
                             let outText = out[0];
                             let specText = spec.out[0]
                             for (let i = 1; i < spec["out"].length; i++) {
