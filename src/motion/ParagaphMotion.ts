4434a1370cb48ce862cb0f225ed1e84990bf977c
74th <site@j74th.com>
2016-11-23 12:12:24 +0800

    paragraph motion

---
+++ src/motion/ParagaphMotion.ts
@@ -0,0 +1,51 @@
+import {AbstractMotion} from "./AbstractMotion";
+import {Position} from "../VimStyle";
+import * as Utils from "../Utils";
+
+export class ParagraphMotion extends AbstractMotion {
+
+    public IsUpDirection: boolean;
+
+    constructor() {
+        super();
+        this.IsUpDirection = false;
+    }
+
+    public CalculateEnd(editor: IEditor, vim: IVimStyle, start: IPosition): IPosition {
+
+        let tabSize = editor.GetTabSize();
+
+        let line = start.Line;
+        let lineText = editor.ReadLine(start.Line);
+        let previousLineIsBlank: boolean = lineText.length == 0;
+        let lastLine = editor.GetLastLineNum();
+        let count = this.Count;
+
+        for (count > 0){
+
+            if (this.IsUpDirection) {
+                line--;
+                if (line == 0) {
+                    break;
+                }
+            } else {
+                line++;
+                if (line == lastLine) {
+                    break;
+                }
+            }
+
+            lineText = editor.ReadLine(line);
+            if (lineText.length == 0 && !previousLineIsBlank) {
+                count--;
+                previousLineIsBlank = true;
+            }else if(lineText.length > 0 ){
+                previousLineIsBlank = false;
+            }
+
+        }
+
+        let end = new Position(line, 0);
+        return editor.UpdateValidPosition(end);
+    }
+}
