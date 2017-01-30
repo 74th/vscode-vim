01dbbc370b7db0c5e9a904aad4990f03b392e12d
74th <site@j74th.com>
2017-01-31 07:36:40 +0900

    add tests

--- test/vim/DeleteTests.ts
+++ test/vim/DeleteTests.ts
@@ -1,10 +1,51 @@
 export let DeleteTests = {};
 
-DeleteTests["delete charactor"] = {
+DeleteTests["Nx:delete N characters under and after the cursor"] = {
     "x:delete charactor": {
         "in": ["a|bc"],
         "key": "x",
         "out": ["a|c"],
+    },
+    "3x:delete 3 charactors": {
+        "in": ["a|bcdef"],
+        "key": "3x",
+        "out": ["a|ef"],
     }
 };
-// dw cw and others are in TextObjectMotion.js
+
+DeleteTests["NX:delete N characters before the cursor"] = {
+    "X:delete previous charactor": {
+        "in": ["ab|c"],
+        "key": "X",
+        "out": ["a|c"],
+    },
+    "3X:delete 3 previous charactors": {
+        "in": ["abcd|ef"],
+        "key": "3X",
+        "out": ["a|ef"],
+    }
+};
+
+// Nd{motion} dw cw and others are in TextObjectMotion.js
+
+// {visual}d
+
+DeleteTests["Ndd:delete N lines"] = {
+    "dd:delete line": {
+        "in": [
+            "abc",
+            "d|ef",
+            "ghe"
+        ],
+        "key": "dd",
+        "out": [
+            "abc",
+            "|ghe"
+        ],
+    },
+    "3dd:delete 3 lines": {
+        "in": ["a", "|b", "c", "d", "e", "f"],
+        "key": "3dd",
+        "out": ["a", "|e", "f"],
+    }
+};
