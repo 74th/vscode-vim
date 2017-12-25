7e7f026776cfb949eb45fe00e3ac3c174d57343a
74th <site@j74th.com>
2017-12-26 08:18:33 +0900

    #68 add diw daw tests

--- test/vim/TextObjects.ts
+++ test/vim/TextObjects.ts
@@ -1,4 +1,112 @@
-export let TextObjects = {};
+export let TextObjects: { [key: string]: { [key: string]: any } } = {};
+
+TextObjects["delete inner word"] = {
+    "diw": {
+        in: ["aaa b|bb ccc ddd"],
+        key: "diw",
+        out: ["aaa | ccc ddd"],
+    },
+    "d2iw": {
+        in: ["aaa b|bb ccc ddd"],
+        key: "d2iw",
+        out: ["aaa |ccc ddd"],
+    },
+    "d3iw": {
+        in: ["aaa b|bb ccc ddd"],
+        key: "d3iw",
+        out: ["aaa | ddd"],
+    },
+    "d2iw for lf": {
+        in: [
+            "aaa b|bb",
+            "ccc ddd",
+        ],
+        key: "d2iw",
+        out: ["aaa | ddd"],
+    },
+    "d2iw for if and space": {
+        in: [
+            "aaa b|bb ",
+            "ccc ddd",
+        ],
+        key: "d2iw",
+        out: [
+            "aaa| ",
+            "ccc ddd",
+        ],
+    },
+    "d3iw for lf": {
+        in: [
+            "aaa b|bb",
+            "ccc ddd",
+        ],
+        key: "d3iw",
+        out: ["aaa |ddd"],
+    },
+    "d3iw for lf and space": {
+        in: [
+            "aaa b|bb ",
+            "ccc ddd",
+        ],
+        key: "d3iw",
+        out: [
+            "aaa | ddd",
+        ],
+    },
+};
+TextObjects["delete outer word"] = {
+    "daw": {
+        in: ["aaa b|bb ccc ddd"],
+        key: "daw",
+        out: ["aaa |ccc ddd"],
+    },
+    "d2aw": {
+        in: ["aaa b|bb ccc ddd"],
+        key: "d2aw",
+        out: ["aaa |ddd"],
+    },
+    "d3aw": {
+        in: ["aaa b|bb ccc ddd"],
+        key: "d3aw",
+        out: ["aa|a"],
+    },
+    "d2aw for lf": {
+        in: [
+            "aaa b|bb",
+            "ccc ddd",
+        ],
+        key: "d2aw",
+        out: ["aaa |ddd"],
+    },
+    "d2aw for if and space": {
+        in: [
+            "aaa b|bb ",
+            "ccc ddd",
+        ],
+        key: "d2aw",
+        out: [
+            "aaa |ddd",
+        ],
+    },
+    "d3aw for lf": {
+        in: [
+            "aaa b|bb",
+            "ccc ddd eee",
+        ],
+        key: "d3aw",
+        out: ["aaa |eee"],
+    },
+    "d3aw for lf and space": {
+        in: [
+            "aaa b|bb ",
+            "ccc ddd eee",
+        ],
+        key: "d3aw",
+        out: [
+            "aaa |eee",
+        ],
+    },
+};
 
 TextObjects["di("] = {
     "di(": {
@@ -112,7 +220,7 @@ TextObjects["di'"] = {
         key: "di`",
         out: ["`  `|` `"],
     },
-}
+};
 
 TextObjects["da'"] = {
     "da'": {
@@ -130,4 +238,4 @@ TextObjects["da'"] = {
         key: "da`",
         out: ["`  |`"],
     },
-}
+};
